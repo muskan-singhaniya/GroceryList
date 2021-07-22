8c551929bb7c6a4ae28487cf5534184ae8ebfc1c
Muskan Singhaniya <70309990+muskan-singhaniya@users.noreply.github.com>
2021-07-22 18:42:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const h1 = document.querySelector('h1');
 const input = document.querySelector('#nameinp');
+const container = document.querySelector("#list");
+
 input.addEventListener('input', e => {
-    if(input.value){
+    if (input.value) {
         h1.innerText = `Hello, ${input.value}`;
     }
-    else{
+    else {
         h1.innerText = "Enter Your Name";
     }
 });
@@ -15,8 +17,15 @@ form.addEventListener('submit', e => {
     const prod = form.elements.product;
     const q = form.elements.qty;
     const li = document.createElement('li');
-    li.innerHTML = ` ${q.value} ${prod.value} `;
-    document.querySelector('#list').appendChild(li);
+    const btn = document.createElement('button');
+    btn.textContent = "Delete"
+    li.innerHTML = ` ${q.value} ${prod.value}        `;
+    li.appendChild(btn);
+    container.appendChild(li);
     prod.value = '';
     q.value = '';
 })
+
+container.addEventListener('click', function (e) {
+    e.target.nodeName === "BUTTON" && e.target.parentElement.remove();
+});
